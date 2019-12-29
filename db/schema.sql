f97bda0217a5e2ffabfd4506d5d89f81407902f8
Zachary <Omitakura@gmail.com>
2019-12-28 18:00:53 -0600

    final update of project

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,15 @@
+DROP DATABASE if exists burgers_db;
+
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers(
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(50) NOT NULL,
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    PRIMARY KEY (id)
+    PRIMARY KEY
+    (id)
 );
